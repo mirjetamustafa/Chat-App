bbc14b7763cd11b4acea15ff374ed42f341dacfb
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2025-08-25 15:09:07 +0200

    chore: install react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import './App.css'
+import ChatRoom from './pages/ChatRoom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <div className="text-red-500">Hello</div>
+  return (
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <ChatRoom />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App
