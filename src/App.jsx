b74ccb782338509f75b5deb4b20f668d4e430831
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2025-08-25 11:59:11 +0200

    chore: install react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div>
-      <h1 className="text-red-500 text-3xl font-bold">Chat App</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h1 className="text-red-500 text-3xl font-bold">Chat App</h1>
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
