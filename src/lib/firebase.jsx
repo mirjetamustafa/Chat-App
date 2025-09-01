a3adae695cae79b38c2abb49a8d0bbc9ae6a10ce
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2025-09-01 11:51:05 +0200

    feat: setup firebase configuration

---
+++ src/lib/firebase.jsx
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDxQA5RDQ9BO5Uw7r3KoXSwZDMA-v64vVQ',
+  authDomain: 'chatapp-7eeda.firebaseapp.com',
+  projectId: 'chatapp-7eeda',
+  storageBucket: 'chatapp-7eeda.firebasestorage.app',
+  messagingSenderId: '1085006158059',
+  appId: '1:1085006158059:web:e2bf4d13ea55a33f5346ae',
+}
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+const auth = getAuth(app)
+const storage = getStorage(app)
+
+export { auth, storage }
+export default db
