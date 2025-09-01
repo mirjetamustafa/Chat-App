416f051ebd0aeb1581a4f9161d9658cb93cd4448
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2025-09-01 12:04:12 +0200

    feat: setup redux toolkit and configuration

---
+++ src/action/index.jsx
@@ -0,0 +1,4 @@
+import { auth } from '../lib/firebase'
+import { SET_USER } from '.actionType'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
---
+++ src/store/index.jsx
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import rootReducer from '../reducer'
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleweare) =>
+    getDefaultMiddleweare({
+      serializableCheck: false,
+    }),
+})
+
+export default store
